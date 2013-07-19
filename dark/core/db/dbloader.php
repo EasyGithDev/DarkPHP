907c8bc15083a84740bc19d8e6632cf0ed2560a6
EasyGithDev <florent.brusciano@gmail.com>
2013-07-19 23:28:17 +0200

    adding the getters and setters

--- dark/core/db/dbloader.php
+++ dark/core/db/dbloader.php
@@ -61,9 +61,9 @@ class DbLoader {
     protected $priority = '';
     protected $local = false;
     protected $insertionType = '';
-    protected $tablename = ''; //where to import to
+    protected $tablename; //where to import to
     protected $filename;  //where to import from
-    protected $headers = array();
+    protected $headers = array(); //list of the headers
     protected $ignoredLines = 1; //number of ignored lines
     protected $fieldSeparateby = ","; //character to separate fields
     protected $fieldEncloseby = "\""; //character to enclose fields, which contain separator char into content
@@ -136,6 +136,132 @@ class DbLoader {
 	    [SET col_name = expr,...]';
 
 	return str_replace($search, $replace, $subject);
+    }
+
+    public function getPriority() {
+	return $this->priority;
+    }
+
+    public function setPriority($priority) {
+	$this->priority = $priority;
+	return $this;
+    }
+
+    public function getLocal() {
+	return $this->local;
+    }
+
+    public function setLocal($local) {
+	$this->local = $local;
+	return $this;
+    }
+
+    public function getInsertionType() {
+	return $this->insertionType;
+    }
+
+    public function setInsertionType($insertionType) {
+	$this->insertionType = $insertionType;
+	return $this;
+    }
+
+    public function getTablename() {
+	return $this->tablename;
+    }
+
+    public function setTablename($tablename) {
+	$this->tablename = $tablename;
+	return $this;
+    }
+
+    public function getFilename() {
+	return $this->filename;
+    }
+
+    public function setFilename($filename) {
+	$this->filename = $filename;
+	return $this;
+    }
+
+    public function getHeaders() {
+	return $this->headers;
+    }
+
+    public function setHeaders($headers) {
+	$this->headers = $headers;
+	return $this;
+    }
+
+    public function getIgnoredLines() {
+	return $this->ignoredLines;
+    }
+
+    public function setIgnoredLines($ignoredLines) {
+	$this->ignoredLines = $ignoredLines;
+	return $this;
+    }
+
+    public function getFieldSeparateby() {
+	return $this->fieldSeparateby;
+    }
+
+    public function setFieldSeparateby($fieldSeparateby) {
+	$this->fieldSeparateby = $fieldSeparateby;
+	return $this;
+    }
+
+    public function getFieldEncloseby() {
+	return $this->fieldEncloseby;
+    }
+
+    public function setFieldEncloseby($fieldEncloseby) {
+	$this->fieldEncloseby = $fieldEncloseby;
+	return $this;
+    }
+
+    public function getFieldEscapeby() {
+	return $this->fieldEscapeby;
+    }
+
+    public function setFieldEscapeby($fieldEscapeby) {
+	$this->fieldEscapeby = $fieldEscapeby;
+	return $this;
+    }
+
+    public function getLineStartingby() {
+	return $this->lineStartingby;
+    }
+
+    public function setLineStartingby($lineStartingby) {
+	$this->lineStartingby = $lineStartingby;
+	return $this;
+    }
+
+    public function getLineTerminatedby() {
+	return $this->lineTerminatedby;
+    }
+
+    public function setLineTerminatedby($lineTerminatedby) {
+	$this->lineTerminatedby = $lineTerminatedby;
+	return $this;
+    }
+
+    public function getCharsetFile() {
+	return $this->charsetFile;
+    }
+
+    public function setCharsetFile($charsetFile) {
+	$this->charsetFile = $charsetFile;
+	return $this;
+    }
+
+    public function getSaticValues() {
+	return $this->saticValues;
+    }
+
+    public function setSaticValues($saticValues) {
+	$this->saticValues = $saticValues;
+	return $this;
     }
 
 }
