59b92313b2814d3cac28ae4e57b100c7fbd60b5a
EasyGithDev <florent.brusciano@gmail.com>
2013-07-21 13:11:57 +0200

    adding the licence

--- dark/core/registry/registry.php
+++ dark/core/registry/registry.php
@@ -1,9 +1,35 @@
 <?php
 
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * DarkPHP - a PHP library of components
+ *
+ * @author      Florent Brusciano
+ * @copyright   2013 Florent Brusciano
+ * @version     1.0.0
+ * @package     Dark\Core
+ *
+ * MIT LICENSE
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
+
 
 namespace Dark\Core\Registry;
 
@@ -23,7 +49,7 @@ class Registry {
 
     public static function create() {
 	if (is_null(self::$instance)) {
-	    self::$instance = new self($filename);
+	    self::$instance = new self();
 	}
 	return self::$instance;
     }
